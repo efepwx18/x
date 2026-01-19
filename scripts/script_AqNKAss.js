353f3b638d8327a42368dc509d71272cb28c259c
Efe <me4141650@gmail.com>
2026-01-19 14:39:17 +0300

    Add files via upload

---
+++ scripts/script_AqNKAss.js
@@ -0,0 +1,23 @@
+const glow = document.querySelector(".mouse-glow");
+
+let mouseX = window.innerWidth / 2;
+let mouseY = window.innerHeight / 2;
+let currentX = mouseX;
+let currentY = mouseY;
+
+window.addEventListener("mousemove", (e) => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+});
+
+function animate() {
+    currentX += (mouseX - currentX) * 0.08;
+    currentY += (mouseY - currentY) * 0.08;
+
+    glow.style.left = `${currentX}px`;
+    glow.style.top = `${currentY}px`;
+
+    requestAnimationFrame(animate);
+}
+
+animate();
